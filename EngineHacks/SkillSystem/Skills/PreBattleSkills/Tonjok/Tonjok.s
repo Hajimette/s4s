2f2a1cdadab5a02d4166980ca89dc17d03e5206a
Hajimette <search4seiso@gmail.com>
2025-10-06 00:45:39 -0700

    No more wormposting

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/Tonjok/Tonjok.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/Tonjok/Tonjok.s
@@ -1,36 +1,49 @@
-.thumb
-.equ TonjokID, SkillTester+4
-.set Tonjok_WindTomeList, SkillTester+8
+.thumb 
 
-push {r4, r5, r6, r14}
+.global Tonjok 
+.type Tonjok, %function 
+Tonjok: 
+push {r4-r7, r14}
 mov r4, r0 @atkr
 
-.set gBattleStats, 0x0203A4D4
+.set gBattleStats, 0x203A4D4
 
 
-Range:
-ldr r1, =gBattleStats
+//Range:
+ldr r1, =#0x203A4D4
 ldrb r2, [r1, #2] // gBattleStats.range
 cmp r1, #1
-bne End // if range != 1 then end
+beq End // if range != 1 then end
 
 
-ldr r0, SkillTester
-mov lr, r0
-mov r0, r4 @attacker data
-ldr r1, TonjokID
-.short 0xf800
+//@check range
+//ldr r0,=#0x203A4D4 @battle stats
+//ldrb r0,[r0,#2] @range
+//cmp r0,#1
+//bne End
+
+//@make sure we are in combat (or combat prep)
+ldrb    r3, =#0x203A4D4
+ldrb    r3, [r3]
+cmp     r3, #4
+beq     End // if range != 0 then end
+
+/*
+mov r0, r4 @attacker data // Checks if lass has Tonjok at 1 range
+ldr r1, =TonjokID 
+lsl r1, #24 
+lsr r1, #24 
+bl SkillTester 
 cmp r0, #0
 beq End
-
-
+*/
 
 LoopInit:
-mov r0, #0xFF
+mov r0, #0xFF //calls 0xFF into r0
 mov r3, #0 // index into the list is r3
 ldr r5, =Tonjok_WindTomeList // the list itself is in r5
 Loop:
-ldrb r6, [r5, r3] // load wind tome ID into r0, check if it's 0 (end of list)
+ldrb r6, [r5, r3] // load wind tome ID into r0, check if it's 0 (end of list) puts the list and the ID together into r6
 cmp r6, #0
 beq EndLoop
   SubLoopInit:
@@ -50,10 +63,12 @@ b Loop
 EndLoop:
 b End
 Break:
-	// If here, you have a wind tome in your inventory
+    // If here, you have a wind tome in your inventory
 
-	// v Damage boost down here
+    // v Damage boost down here
 
+
+/*
 .set GetItemData, 0x080177B0
 // ...
 // bunit ptr in r4, r4 and r14 pushed
@@ -66,12 +81,19 @@ ldr r0, [r0, #0x8]
 mov r1, #0x2
 tst r0, r1
 bne End
+*/
+
+//Adding mag to str
+mov r0, #0x4C    @r0 = BattleUnitWeaponAbilities offset
+ldr r1, [r4, r0] @r1 = BattleUnitWeaponAbilities
+mov r2, #0x2     @r2 = IA_MAGIC
+bic r1, r2       @Clear IA_MAGIC from r1 
+str r1, [r4, r0] @BattleUnitWeaponAbilities = r1
 
 
 End:
-pop {r4-r7, r15}
+pop {r4-r7}
+pop {r0}
+bx r0
 .align
 .ltorg
-SkillTester:
-@Poin SkillTester
-@WORD TonjokID
